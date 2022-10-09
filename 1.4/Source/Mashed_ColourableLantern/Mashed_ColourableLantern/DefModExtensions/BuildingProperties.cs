3d47c8628d21303f5e235bb9a76580719b0ef3b0
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-10-10 09:14:28 +1300

    Lanterns on buildings

---
+++ 1.4/Source/Mashed_ColourableLantern/Mashed_ColourableLantern/DefModExtensions/BuildingProperties.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace Mashed_ColourableLantern
+{
+    public class BuildingProperties : DefModExtension
+    {
+        public bool preventDuplicatePlacement = true;
+
+        public static BuildingProperties Get(Def def)
+        {
+            return def.GetModExtension<BuildingProperties>();
+        }
+    }
+}
--- 1.4/Source/Mashed_ColourableLantern/Mashed_ColourableLantern/Harmony/Harmony.cs
+++ 1.4/Source/Mashed_ColourableLantern/Mashed_ColourableLantern/Harmony/Harmony.cs
@@ -1,6 +1,8 @@
 ﻿using HarmonyLib;
 using Verse;
 using System.Reflection;
+using RimWorld;
+using System.Collections.Generic;
 
 namespace Mashed_ColourableLantern
 {
@@ -9,7 +11,7 @@ namespace Mashed_ColourableLantern
     {
         static Main()
         {
-            var harmony = new Harmony("com.Mashed_ColourableLantern");
+            Harmony harmony = new Harmony("com.Mashed_ColourableLantern");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
     }
@@ -25,6 +27,31 @@ namespace Mashed_ColourableLantern
             if (comp != null)
             {
                 comp.UpdateColour();
+            }
+        }
+
+
+        [HarmonyPatch(typeof(GenConstruct), "CanPlaceBlueprintAt")]
+        public static class GenConstruct_CanPlaceBlueprintOver_Patch
+        {
+            public static void Postfix(ref AcceptanceReport __result, BuildableDef entDef, IntVec3 center, Rot4 rot, Map map)
+            {
+                if (BuildingProperties.Get(entDef) != null && BuildingProperties.Get(entDef).preventDuplicatePlacement)
+                {
+                    foreach (IntVec3 item in GenAdj.OccupiedRect(center, rot, entDef.Size))
+                    {
+                        List<Thing> thingList = item.GetThingList(map);
+                        for (int i = 0; i < thingList.Count; i++)
+                        {
+                            Thing thing2 = thingList[i];
+                            if (BuildingProperties.Get(thing2.def) != null && BuildingProperties.Get(thing2.def).preventDuplicatePlacement)
+                            {
+                                __result = new AcceptanceReport("SpaceAlreadyOccupied".Translate());
+                                return;
+                            }
+                        }
+                    }
+                }
             }
         }
     }
